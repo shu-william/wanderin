3d7cbcac7632dc03d0f555fc522c81744402d1c6
Ryan McMurtry <rkainoam@gmail.com>
2023-09-12 01:46:15 -0700

    created base navbar layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import "./components/styles/styles.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/homepage/Home"
+import Home from "./components/homepage/Home";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes className="app-routes">
         <Route path="/" element={<Home />} />
       </Routes>
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,3 +2,33 @@
 // Button that navigates to Login/Registration page
 // Button that navigates to user's Saves page
 // Button that navigates to Create A New Post page
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <header>
+        <nav className="navbar">
+          <Link to={"/"} className="nav-link mx-3">
+            Home
+          </Link>
+
+          <Link to={"/new"} className="nav-link mx-3">
+            New Post
+          </Link>
+
+          <Link to={"/saves"} className="nav-link mx-3">
+            Saved Posts
+          </Link>
+          
+          <Link to={"/login"} className="nav-link mx-3">
+            Login
+          </Link>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
