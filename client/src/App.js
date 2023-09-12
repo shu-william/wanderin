7910c89b0f03b01164954d705b56d6410fe73ce9
Ryan McMurtry <rkainoam@gmail.com>
2023-09-12 01:16:48 -0700

    Creating homepage component layout. Also edited index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/homepage/Home"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes className="app-routes">
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/homepage/Home.jsx
+++ client/src/components/homepage/Home.jsx
@@ -2,3 +2,14 @@
 // Each post must include a save button and like button
 // Search/Filter bar is displayed above posts
 // Nav bar is displayed at the very top of the page
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>HOMEPAGE</h1>
+    </div>
+  );
+}
+
+export default Home;
